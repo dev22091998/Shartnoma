6c5b44bd2c0f9752874d6c47387adf86cc2d3e75
dev22091998 <forgithub756@gmail.com>
2025-07-22 17:31:33 +0500

    mongoDB ip sozlamalari

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,2 +1,3 @@
-// export const BASE_URL = 'http://localhost:5000';
-export const BASE_URL = 'http://172.16.252.152:8000';
+// export const BASE_URL = 'http://localhost:5000';     
+// export const BASE_URL = 'http://172.16.252.152:8000';  //Meni servwerim
+export const BASE_URL = 'http://172.16.253.180:8000';  //Linux serveri
