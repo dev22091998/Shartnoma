b23b0f6997d8643d76c1a97ca1f595aa65d68aeb
dev22091998 <forgithub756@gmail.com>
2025-07-23 12:00:10 +0500

    IPga otkazdim

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = 'http://localhost:5000';     
 // export const BASE_URL = 'http://172.16.252.152:8000';  //Meni servwerim
 // export const BASE_URL = 'http://172.16.253.180:8000';  //Linux serveri
-export const BASE_URL = 'https://shartnoma.autorubber.uz';
+// export const BASE_URL = 'https://shartnoma.autorubber.uz';
+export const BASE_URL = 'https://172.16.253.180:8000';  //Linux serveri
 
